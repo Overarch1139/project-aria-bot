7bd6536627cd5f310e568c04c6439c391218345d
Echo2124 <echo2124@hotmail.com>
2022-03-28 01:17:26 +1100

    Resolve emote ref bug

--- src/main/java/com/github/echo2124/OnCampus.java
+++ src/main/java/com/github/echo2124/OnCampus.java
@@ -28,14 +28,13 @@ public class OnCampus {
         Runnable task = new Runnable() {
             @Override
             public void run() {
+                String checkUnicode="U+2705";
                 System.out.println("Running task");
                // remove previous msgs & remove role from everyone
                 Role oncampus=Main.constants.jda.getRoleById(Main.constants.ONCAMPUS_ROLE_ID);
                 // recreating role
                 oncampus.createCopy().queue();
                 oncampus.delete().queue();
-                Emote check=Main.constants.jda.getEmoteById("955646106304413697");
-
                 TextChannel msgChannel= Main.constants.jda.getTextChannelById(Main.constants.ONCAMPUS_CHANNEL);
                 // recreating channel
                 msgChannel.createCopy().queue();
@@ -43,11 +42,11 @@ public class OnCampus {
                 // generate msg
                 // consider adding date to this msg
                 msgChannel.sendMessage("React below to the following emoji listed if you are heading to campus today").queue(message -> {
-                    message.addReaction(check);
+                    message.addReaction(checkUnicode);
                     ListenerAdapter s = new ListenerAdapter() {
                         @Override
                         public void onMessageReactionAdd(@NotNull MessageReactionAddEvent event) {
-                            if (event.getMessageId().equals(message.getId()) && event.getReactionEmote().getId().equals(check.getId())) {
+                            if (event.getMessageId().equals(message.getId()) && event.getReactionEmote().getName().equals("white_check_mark")) {
                                 event.getGuild().addRoleToMember(event.getMember(),oncampus);
                             }
                             super.onMessageReactionAdd(event);
