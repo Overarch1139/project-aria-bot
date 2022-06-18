5bf8ea003f014567a0fac04edd80e6726266611b
Echo2124 <echo2124@hotmail.com>
2022-06-19 01:59:58 +1000

    fix out of bounds derp

--- src/main/java/com/github/echo2124/Database.java
+++ src/main/java/com/github/echo2124/Database.java
@@ -281,7 +281,7 @@ public class Database {
                     //TODO check for origin instead (there is probably an issue with the current method of checking for a table which is causing these sorts of problems that exist currently)
                     ResultSet rs = connection.prepareStatement("SELECT EXISTS ( SELECT FROM pg_tables WHERE tablename='EXPOSURE');").executeQuery();
                     while (rs.next()) {
-                        if (rs.getBoolean(0)) {
+                        if (rs.getBoolean(1)) {
                             System.out.println("[Database] checking db for exposure info");
                             sqlQuery = connection.prepareStatement("SELECT len FROM EXPOSURE WHERE origin=?");
                             sqlQuery.setString(1, req);
