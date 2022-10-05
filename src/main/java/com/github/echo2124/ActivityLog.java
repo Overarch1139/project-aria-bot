d527d2ac72f4aaeabc76fbd35ba228815f4064f1
Echo2124 <echo2124@hotmail.com>
2022-10-06 03:09:27 +1100

    finalise refactor of activitylog module

--- src/main/java/com/github/echo2124/ActivityLog.java
+++ src/main/java/com/github/echo2124/ActivityLog.java
@@ -13,44 +13,48 @@ import java.time.format.DateTimeFormatter;
 public class ActivityLog {
     // 1 = info; 2=warn; 3=error;
     public void sendActivityMsg(String msg, int type, String guildID) {
-        // guildID is null then send to all guilds
-        if (guildID==null) {
-        } else {
+        if (guildID!=null) {
             TextChannel msgChannel = Main.constants.jda.getTextChannelById(Main.constants.config.get(guildID).getChannelActivityLogId());
-        }
-        ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Australia/Melbourne"));
-        String concat="```";
-        String endingSymbol="";
-        String tag="";
-        Role role=null;
-        switch (type) {
-            case 1:
-                concat+="yaml\n";
-                tag="[INFO]";
-                break;
-            case 2:
-                concat+="fix\n";
-                tag="[WARN]";
-                break;
-            case 3:
-                concat+="diff\n";
-                concat+="- ";
-                tag="[ERROR]";
-                endingSymbol=" - ";
-                role=Main.constants.jda.getRoleById(Main.constants.config.getRoleDevTeamId());
-                break;
-            default:
-        }
-        DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm:ss Z");
-        concat+="["+now.format(format)+"]";
-        concat+=tag;
-        concat+=msg;
-        concat+=endingSymbol;
-        concat+="\n```";
-        if (user!=null) {
-            msgChannel.sendMessage(concat+"<@&"+Main.constants.config.getRoleDevTeamId()+">").queue();
+            ZonedDateTime now = ZonedDateTime.now(ZoneId.of("Australia/Melbourne"));
+            String concat="```";
+            String endingSymbol="";
+            String tag="";
+            Role role=null;
+            switch (type) {
+                case 1:
+                    concat+="yaml\n";
+                    tag="[INFO]";
+                    break;
+                case 2:
+                    concat+="fix\n";
+                    tag="[WARN]";
+                    break;
+                case 3:
+                    concat+="diff\n";
+                    concat+="- ";
+                    tag="[ERROR]";
+                    endingSymbol=" - ";
+                    role=Main.constants.jda.getRoleById(Main.constants.config.get(guildID).getRoleDevTeamId());
+                    break;
+                default:
+            }
+            DateTimeFormatter format = DateTimeFormatter.ofPattern("dd/MM/yyyy - HH:mm:ss Z");
+            concat+="["+now.format(format)+"]";
+            concat+=tag;
+            concat+=msg;
+            concat+=endingSymbol;
+            concat+="\n```";
+            if (role!=null) {
+                msgChannel.sendMessage(concat+"<@&"+Main.constants.config.get(guildID).getRoleDevTeamId()+">").queue();
+            } else {
+                msgChannel.sendMessage(concat).queue();
+            }
         } else {
-            msgChannel.sendMessage(concat).queue();
+            // sends to all guilds
+            for (String key: Main.constants.config.keySet()) {
+                sendActivityMsg(msg, type, key);
+            }
         }
     }
+
 }
