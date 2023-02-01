cd1225765b3c177d99e4fdef2ebae630f505d0fd
Echo2124 <echo2124@hotmail.com>
2023-02-02 00:41:45 +1100

    finialise test constructor for sheetparser

--- src/main/java/com/github/echo2124/SheetParser.java
+++ src/main/java/com/github/echo2124/SheetParser.java
@@ -8,8 +8,7 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.ss.util.CellReference;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 
-import java.io.File;
-import java.io.InputStream;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.concurrent.CompletableFuture;
@@ -39,44 +38,53 @@ public class SheetParser {
 
     // test constructor
     public SheetParser() {
-        InputStream stream=null;
-        String fileName="";
-        this.serverId=null;
-        CompletableFuture<InputStream> futureStream = new CompletableFuture<InputStream>();
-        // using input stream instead of file due to given file not being stored & current file impl being changed in DiscordJDA
+        // todo implement direct file input (for testing purpose)
+        File file= new File("src/main/java/com/github/echo2124/test.xls");
+        InputStream targetStream=null;
         try {
-            fileName=msgattached.getFileName();
-            msgattached.getProxy().download();
-            parser(futureStream.get());
-        } catch (Exception e) {
-            activityLog.sendActivityMsg("[SHEET_PARSER] "+e.getMessage(),3, null);
-
+            targetStream = new FileInputStream(file);
+        } catch (FileNotFoundException e) {
+            System.out.println(e.getMessage());
         }
+        this.serverId=null;
+        parser(targetStream);
     }
 
     public void parser(InputStream msgattached) {
         try {
             Workbook workbook = new XSSFWorkbook(msgattached);
             Sheet sheet = workbook.getSheetAt(0);
-            HashMap<Integer, ArrayList<String>> data;
+
             /* TODO:
              -- (1) determine each column and index them for next part
              -- (2) iterate through each row and grab needed information to cross reference
              */
-            // -- (1)
-
-            // -- (2)
             int i=0;
             for (Row row : sheet) {
+                HashMap<String, String> data = new HashMap<String, String>();
                 for (Cell cell : row) {
                     String columnName=getCellName(cell);
                     String[] parentColumns=Main.constants.config.get(serverId).getSheetParserParentColumns();
                     for (int a=0; a<parentColumns.length; a++) {
                         if (columnName.contains(parentColumns[a])) {
-
+                            String cellContents=cell.getStringCellValue();
+                            if (cell.getStringCellValue().isEmpty()) {
+                                cellContents="null";
+                            }
+                            // convert to db column format
+                            if (parentColumns[a].toLowerCase().contains("email")) {
+                                data.put("emailAddr", cellContents);
+                            }
+                            if (parentColumns[a].toLowerCase().contains("name")) {
+                                data.put("name", cellContents);
+                            }
                         }
                     }
                 }
+                // debug
+                data.entrySet().forEach(entry -> {
+                    System.out.println(entry.getKey() + " " + entry.getValue());
+                });
                 i++;
             }
         } catch (Exception e) {
